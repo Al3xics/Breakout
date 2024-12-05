4aa87afb9f6ebd50a5b10625d9c34482f1df8cec
Alexis <alexis.martinho.78@gmail.com>
2024-12-05 17:13:33 +0100

    Input Mapping

---
+++ Source/Breakout/Private/BKBrick.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BKBrick.h"
+
+
+// Sets default values
+ABKBrick::ABKBrick()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABKBrick::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABKBrick::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
